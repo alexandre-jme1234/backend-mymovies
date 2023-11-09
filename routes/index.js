7b1b1f160b9b0ea497a7a51da564b02eed054b29
jammealexandre28 <jammealexandre1@gmail.com>
2023-11-09 16:41:45 +0100

    first commit

--- routes/index.js
+++ routes/index.js
@@ -1,21 +1,24 @@
 var express = require('express');
 var router = express.Router();
+
+const API_KEY = process.env.TMDB_API_KEY;
+
 const fetch = require("node-fetch");
 
-const TMDB_API_KEY = process.env.TMDB_API_KEY;
-
-
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/movies', function(req, res) {
+  fetch(`https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}`)
+  .then(response => response.json())
+  .then(data => {
+    console.log(data)
+    // const movies = data.results.map((data, i) => {
+    //   if(data.overview.length >= 250) {
+    //     data.overview = data.overview.substr(0, 249)+'...'
+    // }
+    //   return ({data})
+    // })
+  })
+  res.json({movies})
 });
-
-router.get('/movies', (req, res) => {
-  fetch(`https://api.themoviedb.org/3/discover/movie?api_key=${TMDB_API_KEY}`)
-    .then(response => response.json())
-    .then(data => {
-      res.json({ movies: data });
-    });
- });
 
 module.exports = router;
